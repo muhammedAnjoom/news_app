ba3736b888279d3159658b48528c26b67e61515b
muhammedAnjoom <anjoompa42003@gmail.com>
2022-12-04 20:25:52 +0530

    custom bottm bar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:news_app/style/app_style.dart';
 
+
 import 'view/home/home_screen.dart';
+import 'view/widgets/custom_bottom_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,24 +14,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent
-      )
-    );
-    return  MaterialApp(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:const Scaffold(
+      home: Scaffold(
         backgroundColor: kLighterWhite,
-        body: HomeScreen(),
+        body: const HomeScreen(),
+        bottomNavigationBar: CustomBottomBar(),
       ),
-      theme: ThemeData(
-        primaryColor: Colors.blue
-      ),
+      theme: ThemeData(primaryColor: Colors.blue),
     );
   }
 }
-
-
---
+++ lib/view/widgets/custom_bottom_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../style/app_style.dart';
+
+class CustomBottomBar extends StatefulWidget {
+  CustomBottomBar({
+    super.key,
+  });
+
+  
+  @override
+  State<CustomBottomBar> createState() => _CustomBottomBarState();
+}
+
+class _CustomBottomBarState extends State<CustomBottomBar> {
+
+  int _selectedIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // void _onItemTapped(int index) {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      elevation: 0,
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: kwhite,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: _selectedIndex == 0
+              ? SvgPicture.asset('assets/icons/home.svg',height: 25,width: 25,)
+              : SvgPicture.asset('assets/icons/home_un.svg',height: 25,width: 25,),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: _selectedIndex == 1
+              ? SvgPicture.asset('assets/icons/bookmark.svg')
+              : SvgPicture.asset('assets/icons/bookmark_un.svg'),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: _selectedIndex == 2
+              ? SvgPicture.asset('assets/icons/notification.svg')
+              : SvgPicture.asset('assets/icons/notification_un.svg'),
+          label: '',
+        ),
+        
+        BottomNavigationBarItem(
+          icon: _selectedIndex == 3
+              ? SvgPicture.asset('assets/icons/user.svg',height: 25,width: 25,)
+              : SvgPicture.asset('assets/icons/user_un.svg',height: 25,width: 25,),
+          label: '',
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    );
+  }
+}
